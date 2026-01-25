9a035acc3a5a341c969a9617f4ba85dffacdc10f
AbuObaida01 <obaidaabu5200@gmail.com>
2026-01-25 11:38:39 +0530

    add oauth2 login with jwt authentication

--- app/router/auth.py
+++ app/router/auth.py
@@ -1,21 +1,24 @@
 from fastapi import FastAPI, Response, status, Depends, APIRouter, HTTPException
-from .. import database, models, schemas, utils
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from .. import database, models, schemas, utils, oauth2
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 
 router=APIRouter(tags=["Authentication"])
 
 @router.post("/login")
-def get_login(user_credentials: schemas.UserLogin, db: Session=Depends(database.get_db)):
-    stmt=select(models.User).where(models.User.email==user_credentials.email)
+def get_login(user_credentials: OAuth2PasswordRequestForm=Depends(), db: Session=Depends(database.get_db)):
+    stmt=select(models.User).where(models.User.email==user_credentials.username)
     user=db.execute(stmt).scalar_one_or_none()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials")
     
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials")
 
-    # ROuter
+    # create a token and return
+    access_token=oauth2.create_access_token(data={"user_id":user.id})
 
-    return {"message": "Successfully logged in"}
+
+    return {"access_token":access_token, "token_type":"bearer"}
